e3a164e9ddf76b873c9d0a74e37a8e773a575f0d
Michael Clinton <michael.john.clinton@gmail.com>
2024-12-10 16:20:07 -0500

    removed uneeded file

--- .mono/temp/obj/Debug/Our Ruinous Gods.AssemblyInfo.cs
+++ .mono/temp/obj/Debug/Our Ruinous Gods.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Our Ruinous Gods")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+044c1f413135e3b05f19e46752ba2f60200a14aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+134efb3ae527b96ada10a4568b5a203592eb4beb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Our Ruinous Gods")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Our Ruinous Gods")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
