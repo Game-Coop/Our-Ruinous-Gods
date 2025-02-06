2f9361f77fb766621990f66444d7a2e0422238de
Michael Clinton <michael.john.clinton@gmail.com>
2025-02-06 15:56:33 -0500

    Player movement and gravity mostly correct just need to fix down direction

--- scripts/player.cs
+++ scripts/player.cs
@@ -2,17 +2,16 @@ using Godot;
 using System;
 using System.IO.Compression;
 using System.Security.Permissions;
+using System.Xml.Schema;
 
 public class player : KinematicBody
 {
     [Export] private float gamepad_sensitivity = 1;
     [Export] private float gravity_radius = 10;  
     [Export] private float gravity = 9.8f;   
-    [Export] private float inertia = 1; 
     [Export] private float mouse_sensitivity = 1;
     [Export] private float speed = 1;
 
-    private Vector3 direction;
     private Spatial head;
     private Vector3 velocity;
 
@@ -22,75 +21,70 @@ public class player : KinematicBody
 
         Input.MouseMode = Input.MouseModeEnum.Captured;
     }
-
     public override void _Input(InputEvent e)
     {
-        if(e is InputEventMouseMotion) handleMouseLook(e as InputEventMouseMotion);
         if (e.IsActionPressed("quit")) GetTree().Quit();
+        if (e is InputEventMouseMotion) handleMouseLook(e as InputEventMouseMotion);
     }
-
     public override void _PhysicsProcess(float delta) {
         float turnHorizontal = (Input.GetActionStrength("look_right") - Input.GetActionStrength("look_left")) * gamepad_sensitivity;
         float turnVertical = (Input.GetActionStrength("look_down") - Input.GetActionStrength("look_up")) * gamepad_sensitivity;
 
         HandelMove(delta);
-        HandelTurn(turnHorizontal, turnVertical);
+        HandelLook(turnHorizontal, turnVertical);
     }
+     private Vector3 GetUpDirection() {
+        Vector3 angleVector = new Vector3(0, 0, GlobalPosition.z);
 
-    private Vector3 GetUpAngle() {
-        if(GlobalPosition.DistanceTo(Vector3.Zero) > gravity_radius) 
-            return GlobalPosition.DirectionTo(Vector3.Zero);
+        if(GlobalPosition.DistanceTo(angleVector) > gravity_radius) 
+            return GlobalPosition.DirectionTo(angleVector).Normalized();
 
         return Vector3.Up;
     }
+    private Vector3 GetDownDirection() {
+        Vector3 angleVector = new Vector3(0, 0, GlobalPosition.z);
 
-    private Vector3 GetGravity(float delta) {
-        /*
-        Vector3 test = Vector3.Zero.DirectionTo(GlobalPosition);
-        
-        if(GlobalPosition.DistanceTo(Vector3.Zero) > gravity_radius) 
-            return test * gravity * delta;
-        */
+        if(GlobalPosition.DistanceTo(angleVector) > gravity_radius) 
+            return angleVector.DirectionTo(GlobalPosition).Normalized();
 
-        return Vector3.Down * gravity * delta;
+        return Vector3.Down;
     }
-    private float TEST(Vector2 a, Vector2 b) {
-    	Vector2 diff = new Vector2(a.x - b.x, a.y - b.y);
-    
-    	return Mathf.Atan2(diff.y, diff.x);
-    }
-
     private void HandelMove(float delta) {
-        float horizontalRotion = GlobalTransform.basis.GetEuler().y;
-        float movement = Input.GetActionStrength("move_backward") - Input.GetActionStrength("move_forward");
-        float strafe = Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left");
+        float velocity_y;
 
-        direction = Vector3.Zero;
-        direction = new Vector3(strafe, 0, movement);
-        direction = direction.Rotated(GetUpAngle(), horizontalRotion).Normalized();
-
-        velocity = velocity.LinearInterpolate(direction * speed, delta / inertia);
-
-        if(GlobalPosition.DistanceTo(Vector3.Zero) > gravity_radius) {
-            GD.Print(Mathf.Rad2Deg(TEST(Vector2.Zero, ));
-            Rotate(GlobalPosition.DirectionTo(Vector3.Zero), Mathf.Rad2Deg(TEST(Vector2.Zero, new Vector2(GlobalPosition.z, GlobalPosition.y))));
-            RotateX(0.0f);
-            /*Rotate(GlobalPosition, GlobalPosition.SignedAngleTo(Vector3.Zero, GetUpAngle()));*/    
+        if(GlobalPosition.DistanceTo(new Vector3(0, 0, GlobalPosition.z)) > gravity_radius) {
+            Transform up_transform = AlignPlayerUp(GlobalTransform, GlobalPosition.DirectionTo(new Vector3(0, 0, GlobalPosition.z)));
+        
+            GlobalTransform = up_transform;
         }
 
-        MoveAndSlide(velocity + GetGravity(delta), GetUpAngle());
-    }
+        velocity += GetDownDirection() * gravity * delta;
+        
+        velocity_y = velocity.y;
 
-    private void HandelTurn(float x, float y) {
+        velocity = Vector3.Zero;
+        velocity += GlobalTransform.basis.z * ((Input.GetActionStrength("move_backward") - Input.GetActionStrength("move_forward")) * (speed * 250) * delta);
+        velocity += GlobalTransform.basis.x * ((Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left")) * (speed * 250) * delta);
+        velocity.y = velocity_y;
+
+        velocity = MoveAndSlideWithSnap(velocity, GetDownDirection() * 2, GetUpDirection());
+    }
+    private Transform AlignPlayerUp(Transform transform, Vector3 y) {
+        transform.basis.y = y;
+        transform.basis.x = -transform.basis.z.Cross(y);
+        transform.basis = transform.basis.Orthonormalized();
+
+        return transform;
+    }
+    private void HandelLook(float x, float y) {
         float clampDegrees = 90;
 
-        RotateY(Mathf.Deg2Rad(-x));
+        RotateObjectLocal(Vector3.Up, Mathf.Deg2Rad(-x));
 
         head.RotateX(Mathf.Deg2Rad(-y));
         head.Rotation = new Vector3(Mathf.Clamp(head.Rotation.x, Mathf.Deg2Rad(-clampDegrees), Mathf.Deg2Rad(clampDegrees)), head.Rotation.y, head.Rotation.z);
     }
-
     private void handleMouseLook(InputEventMouseMotion e) {
-        HandelTurn(e.Relative.x * mouse_sensitivity, e.Relative.y * mouse_sensitivity);
+        HandelLook(e.Relative.x * mouse_sensitivity, e.Relative.y * mouse_sensitivity);
     }
 }
