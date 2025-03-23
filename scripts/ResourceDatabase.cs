44b7ed304c897b06993325dd97b2f8b29fade03b
Joseph Star <>
2025-03-23 17:07:20 +0300

    - added resource database

---
+++ scripts/ResourceDatabase.cs
@@ -0,0 +1,35 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+public class ResourceDatabase : Node
+{
+	[Export] private ItemData[] itemDatas;
+	[Export] private JournalData[] journalDatas;
+	[Export] private AudioData[] audioDatas;
+
+	public static Dictionary<int, ItemData> ItemDatas { get; private set; } = new Dictionary<int, ItemData>();
+	public static Dictionary<int, JournalData> JournalDatas { get; private set; } = new Dictionary<int, JournalData>();
+	public static Dictionary<int, AudioData> AudioDatas { get; private set; } = new Dictionary<int, AudioData>();
+
+	public override void _Ready()
+	{
+		base._Ready();
+		ItemDatas.Clear();
+		JournalDatas.Clear();
+		AudioDatas.Clear();
+		foreach (var item in itemDatas)
+		{
+			ItemDatas.Add(item.Id, item);
+		}
+		foreach (var journalData in journalDatas)
+		{
+			JournalDatas.Add(journalData.Id, journalData);
+		}
+		foreach (var audioData in audioDatas)
+		{
+			AudioDatas.Add(audioData.Id, audioData);
+		}
+	}
+
+}
