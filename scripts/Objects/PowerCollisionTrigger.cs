39f81a12761265389c722131da5dd73dfa1b095e
Michael Clinton <michael.john.clinton@gmail.com>
2025-04-26 18:09:33 -0400

    Added power switches and and event

--- scripts/Objects/PowerCollisionTrigger.cs
+++ scripts/Objects/PowerCollisionTrigger.cs
@@ -4,17 +4,23 @@ using System;
 public class PowerCollisionTrigger : Spatial
 {
 	[Export] public int Zone { get; set; }
+	[Export] public bool RunOnce { get; set; }
 	private Area area;
 	public override void _Ready()
 	{
 		area = GetNode<Area>("Area");
 		area.Monitoring = true;
 		area.Connect("body_entered", this, "OnBodyEntered");
-		area.Connect("area_entered", this, "OnBodyEntered");
 	}
 	public void OnBodyEntered(Node body)
 	{
-		EventBus EventBusHandler = GetNode<EventBus>("/root/EventBus");      
-        EventBusHandler.OnPowerEvent(this.Zone);
+		if(body is player) {
+			EventBus EventBusHandler = GetNode<EventBus>("/root/EventBus");      
+			EventBusHandler.OnPowerEvent(this.Zone);
+
+			if(RunOnce) {
+				this.QueueFree();
+			}
+		}
 	}
 }
