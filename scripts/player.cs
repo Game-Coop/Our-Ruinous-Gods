b44dde19db838eebb3a8acc95b20d9496aa35b4c
Michael Clinton <michael.john.clinton@gmail.com>
2025-05-27 16:07:23 -0400

    basic player movement working again after upgrade

--- scripts/player.cs
+++ scripts/player.cs
@@ -1,14 +1,15 @@
 using Godot;
 using System;
+using System.Data.Common;
 using System.IO.Compression;
-
 public partial class Player : CharacterBody3D
 {
 	private Vector3 direction;
 	[Export] private float gamepad_sensitivity = (float)1;
-	private Node3D head;
-	[Export] private float mouse_sensitivity = (float)1;
-    [Export] private float gravity = 9.8f;  
+	private Node3D _head;
+	private Node3D _camera;
+	[Export] private float mouse_sensitivity = (float)0.001;
+    [Export] public float gravity = 9.8f;  
 	[Export] private float speed = 1;
 	[Export] private float inertia = 1;
 	private Vector3 velocity;
@@ -18,9 +19,10 @@ public partial class Player : CharacterBody3D
 
     public override void _Ready()
     {;
-        head = GetNode<Node3D>("Head");
+        _head = GetNode<Node3D>("Head");
+        _camera = GetNode<Node3D>("Head/Camera3D");
 
-        Input.MouseMode = Input.MouseModeEnum.Captured;
+		Input.MouseMode = Input.MouseModeEnum.Captured;
 
         EventBus EventBusHandler = GetNode<EventBus>("/root/EventBus");
         EventBusHandler.Connect("PowerChangedEventHandler", new Callable(this, "OnPowerChange"));
@@ -28,23 +30,61 @@ public partial class Player : CharacterBody3D
         EventBusHandler.Connect("WorldEventHandler", new Callable(this, "OnWorldEvent"));
     }
 	
-	public override void _Input(InputEvent e)
+	public override void _Input(InputEvent @event)
 	{
-		if (e is InputEventMouseMotion) handleMouseLook(e as InputEventMouseMotion);
-		// if (e.IsActionPressed("quit")) GetTree().Quit();
-	}
+		if (@event is InputEventMouseMotion e)
+		{
+			_head.RotateY(-e.Relative.X * mouse_sensitivity);
 
-	public void OnPowerChange(PowerEvent e) {
+			_camera.RotateX(-e.Relative.Y * mouse_sensitivity);
+			_camera.Rotation = new Vector3(Mathf.Clamp(_camera.Rotation.X, Mathf.DegToRad(-80f), Mathf.DegToRad(90f)), _camera.Rotation.Y, _camera.Rotation.Z);
+		}
+
+		if (@event.IsActionPressed("quit")) GetTree().Quit();
+	}
+	public override void _PhysicsProcess(double delta) {
+		//float turnHorizontal = (Input.GetActionStrength("look_right") - Input.GetActionStrength("look_left")) * gamepad_sensitivity;
+		//float turnVertical = (Input.GetActionStrength("look_down") - Input.GetActionStrength("look_up")) * gamepad_sensitivity;
+		//HandelTurn(turnHorizontal, turnVertical);
+
+		Vector3 velocity = Velocity;
+
+		if (!IsOnFloor())
+			velocity.Y -= gravity * (float)delta;
+
+		Vector2 inputDirection = Input.GetVector("move_left", "move_right", "move_forward", "move_backward");
+		Vector3 playerDirection = (_head.GlobalTransform.Basis * new Vector3(inputDirection.X, 0, inputDirection.Y)).Normalized();
+
+		if (playerDirection != Vector3.Zero)
+		{
+			velocity.X = playerDirection.X * speed * (Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left"));
+			velocity.Z = playerDirection.Z * speed;
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, speed);
+			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, speed);
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+	
+	public void OnPowerChange(PowerEvent e)
+	{
 		int currentPower = Power;
 
-		if(e.State == PowerState.On) {
+		if (e.State == PowerState.On)
+		{
 			currentPower += e.Charge;
-		} else {
+		}
+		else
+		{
 			currentPower -= e.Charge;
 		}
 
-		if(currentPower < 0) currentPower = 0;
-		if(currentPower >= MaxPower) currentPower = MaxPower;
+		if (currentPower < 0) currentPower = 0;
+		if (currentPower >= MaxPower) currentPower = MaxPower;
 
 		Power = currentPower;
 		GD.Print("current power use is: " + Power);
@@ -57,46 +97,5 @@ public partial class Player : CharacterBody3D
 	public void OnStaminaChange(int cost) {
 		this.Stamina -= cost;
 		GD.Print("current stamina: " + this.Stamina);
-	}
-
-	public override void _PhysicsProcess(double delta) {
-		float turnHorizontal = (Input.GetActionStrength("look_right") - Input.GetActionStrength("look_left")) * gamepad_sensitivity;
-		float turnVertical = (Input.GetActionStrength("look_down") - Input.GetActionStrength("look_up")) * gamepad_sensitivity;
-
-		HandelMove(delta);
-		HandelTurn(turnHorizontal, turnVertical);
-	}
-
-	private void HandelMove(double delta) {
-		float horizontalRotion = GlobalTransform.Basis.GetEuler().Y;
-		float movement = Input.GetActionStrength("move_backward") - Input.GetActionStrength("move_forward");
-		float strafe = Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left");
-
-		Vector3 playerVelocity = Velocity;
-
-		direction = Vector3.Zero;
-		direction = new Vector3(strafe, 0, movement);
-		direction = direction.Rotated(Vector3.Up, horizontalRotion).Normalized();
-		
-        playerVelocity += Vector3.Down * gravity * (float)delta;
-		playerVelocity = velocity.Lerp(direction * speed, (float)delta / inertia);
-
-		Velocity = playerVelocity;
-
-
-		MoveAndSlide();
-	}
-
-	private void HandelTurn(float x, float y) {
-		float clampDegrees = 90;
-
-		RotateY(Mathf.DegToRad(-x));
-		
-		head.RotateX(Mathf.DegToRad(-y));
-		head.Rotation = new Vector3(Mathf.Clamp(head.Rotation.X, Mathf.DegToRad(-clampDegrees), Mathf.DegToRad(clampDegrees)), head.Rotation.Y, head.Rotation.Z);
-	}
-
-	private void handleMouseLook(InputEventMouseMotion e) {
-		HandelTurn(e.Relative.X * mouse_sensitivity, e.Relative.Y * mouse_sensitivity);
 	}
 }
