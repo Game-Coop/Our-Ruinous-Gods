be3d7efe368b18551dd34bee9331fba254e5c72d
JosephStar318 <https://github.com>
2025-05-30 21:02:02 +0300

    - fbx errors fix and signal fixes

--- scripts/EventBus.cs
+++ scripts/EventBus.cs
@@ -1,27 +1,28 @@
 using Godot;
-public partial class EventBus : Node {
-    [Signal] 
-    public delegate void PowerEventHandler(int zone);
-    [Signal] 
-    public delegate void PowerChangedEventHandler(PowerEvent e);
-    [Signal]
-    public delegate void StaminaChangeEventHandler(int cost);
-    [Signal]
-    public delegate void WorldEventHandler(string name);
-    public void OnPowerEvent(int zone)
-    {
-        EmitSignal("PowerEventHandler", zone);
-    }
-    public void OnPowerChangeEvent(PowerEvent e)
-    {
-        EmitSignal("PowerChangedEventHandler", e);
-    }
-    public void OnStaminaChangeEvent(int cost)
-    {
-        EmitSignal("StaminaChangeEventHandler", cost);
-    }
-    public void OnWorldEvent(string name)
-    {
-        EmitSignal("WorldEventHandler", name);
-    }
+public partial class EventBus : Node
+{
+	[Signal]
+	public delegate void PowerEventHandler(int zone);
+	[Signal]
+	public delegate void PowerChangedEventHandler(PowerEvent e);
+	[Signal]
+	public delegate void StaminaChangeEventHandler(int cost);
+	[Signal]
+	public delegate void WorldEventHandler(string name);
+	public void OnPowerEvent(int zone)
+	{
+		EmitSignal(SignalName.Power, zone);
+	}
+	public void OnPowerChangeEvent(PowerEvent e)
+	{
+		EmitSignal(SignalName.PowerChanged, e);
+	}
+	public void OnStaminaChangeEvent(int cost)
+	{
+		EmitSignal(SignalName.StaminaChange, cost);
+	}
+	public void OnWorldEvent(string name)
+	{
+		EmitSignal(SignalName.World, name);
+	}
 }
