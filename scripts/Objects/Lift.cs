ed7d47064a96a36913bdf5f99e9adc7ab351531f
Charlie <eikascharlie@gmail.com>
2025-08-26 19:37:10 +0200

    pushing objects for further testing

--- scripts/Objects/Lift.cs
+++ scripts/Objects/Lift.cs
@@ -4,28 +4,34 @@ using System;
 public partial class Lift : Node3D
 {
     [Export] public string puzzleId = "lift_puzzle"; //ADD the actual puzzle ID here
-    [Export] public string upAnimation = "lift_up";
-    [Export] public string downAnimation = "lift_down";
+    /*[Export] public string upAnimation = "lift_up";
+    [Export] public string downAnimation = "lift_down";*/
+
+    [Export] public Vector3 upPosition = new Vector3(0, 5, 0);
+    [Export] public Vector3 downPosition = new Vector3(0, 0, 0);
+    [Export] public float moveSpeed = 3f;
 
     private bool isPuzzleSolved = false;
     private bool isUp = true;
+    private bool isMoving = false;
+
     private AnimationPlayer animationPlayer;
+    private Vector3 targetPosition;
     private Area3D interactArea;
 
     public override void _Ready()
     {
-        animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+        //animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+        GlobalPosition = upPosition;
         interactArea = GetNode<Area3D>("InteractArea");
 
-        isUp = true; // Start with the lift in the up position
-
-        EventBus eventBus = GetNode<EventBus>("/root/EventBus");
+        var eventBus = GetNode<EventBus>("/root/EventBus");
         eventBus.PuzzleSolved += OnPuzzleSolved;
     }
 
-    private void OnPuzzleSolved(string puzzleId)
+    private void OnPuzzleSolved(string solvedPuzzleId)
     {
-        if (this.puzzleId == puzzleId)
+        if (solvedPuzzleId == puzzleId)
         {
             isPuzzleSolved = true;
             GD.Print($"Puzzle {puzzleId} solved. Lift unlocked.");
@@ -36,15 +42,20 @@ public partial class Lift : Node3D
     {
         if (!isPuzzleSolved) return;
 
-        if (Input.IsActionJustPressed("interact") && IsPlayerInRange())
+        if (Input.IsActionJustPressed("interact") && IsPlayerInRange() && !isMoving)
         {
             ToggleLift();
+        }
+
+        if (isMoving)
+        {
+            MoveLift(delta);
         }
     }
 
     private void ToggleLift()
     {
-        if (animationPlayer.IsPlaying()) return;
+        /*if (animationPlayer.IsPlaying()) return;
 
         if (isUp)
         {
@@ -55,7 +66,29 @@ public partial class Lift : Node3D
             animationPlayer.Play(upAnimation);
         }
 
-        isUp = !isUp;
+        isUp = !isUp;*/
+
+        targetPosition = isUp ? downPosition : upPosition;
+        isMoving = true;
+    }
+
+    private void MoveLift(double delta)
+    {
+        Vector3 currentPosition = GlobalPosition;
+        Vector3 direction = (targetPosition - currentPosition).Normalized();
+        float distance = (targetPosition - currentPosition).Length();
+        float moveStep = moveSpeed * (float)delta;
+
+        if (moveStep >= distance)
+        {
+            GlobalPosition = targetPosition;
+            isUp = !isUp;
+            isMoving = false;
+        }
+        else
+        {
+            GlobalPosition += direction * moveStep;
+        }
     }
 
     private bool IsPlayerInRange()
