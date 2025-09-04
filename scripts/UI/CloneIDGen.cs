76cdac9ddb9e0fe3b2469d5e05efc9d2853254b4
XorsDomain <110067216+XorsDomain@users.noreply.github.com>
2025-09-04 18:54:48 -0400

    removed commented code

--- scripts/UI/CloneIDGen.cs
+++ scripts/UI/CloneIDGen.cs
@@ -42,10 +42,4 @@ public class CloneIDGen : CanvasLayer
 		
 		return id;
 	}
-
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
 }
