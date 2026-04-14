6badc05f29cf9baa72dd2a1626c3f69ad116250e
Michael Greenhut <michael@playmatics.com>
2026-04-14 08:20:48 -0400

    test dialogue
    Testing the docking bay initial quest dialogue

--- scripts/Parley/ParleyTest.cs
+++ scripts/Parley/ParleyTest.cs
@@ -2,16 +2,16 @@ using Godot;
 
 public partial class ParleyTest : Node
 {
-    [Export] private Resource dialog;
+	[Export] private Resource dialog;
 
-    public override void _Ready()
-    {
-        base._Ready();
-        ParleyFacts.SetValue("isAlive", false);
-        CallDeferred(nameof(RunDialog));
-    }
-    public void RunDialog()
-    {
-        Parley.RunDialog(dialog);
-    }
+	public override void _Ready()
+	{
+		base._Ready();
+		ParleyFacts.SetValue("isAlive", false);
+		CallDeferred(nameof(RunDialog));
+	}
+	public void RunDialog()
+	{
+		Parley.RunDialog(dialog);
+	}
 }
